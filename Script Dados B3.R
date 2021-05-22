fd5ddd564c7ed579ea9b832ade989ff334883d4f
Leonardo Chalhoub <leochalhoub@hotmail.com>
2021-05-22 17:15:14 -0300

    v2

--- Script Dados B3.R
+++ Script Dados B3.R
@@ -15,7 +15,18 @@ l_dfp <- get_dfp_data(companies_cvm_codes = NULL,
                       cache_folder = tempdir(), # use local folder in live code
                       type_docs = c("BPA", "BPP", "DRE"),
                       type_format = 'con',
-                      first_year = 2000, 
-                      last_year = lubridate::year(Sys.Date()))
+                      first_year = 2010, 
+                      last_year = 2020)
 
 str(l_dfp)
+
+l_dfp_EM <- l_dfp %>% 
+  
+
+writexl::write_xlsx(x = list(df_info = df_info),
+                    path = 'D:/Desktop/FINOR/DadosEmpresasB3.xlsx',
+                    format_headers = F)
+writexl::write_xlsx(x = list(DRE = l_dfp$`DF Consolidado - Demonstração do Resultado`),
+                    path = 'D:/Desktop/FINOR/l_dfp.xlsx',
+                    format_headers = F)
+                    
